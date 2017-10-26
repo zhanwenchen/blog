42b1c75ebff2eb0fa61d6f3de6f549f8361770d7
Zhanwen Chen <phil@zhanwenchen.com>
2017-10-26 17:33:25 -0400

    flipped react-router Routes 'exact' positions to reflect parent and children routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
 import Home from './views/Home';
 import Login from './views/Login';
+import CreatePost from './views/CreatePost';
 import './App.css';
 
 export default class App extends React.Component {
@@ -12,8 +13,9 @@ export default class App extends React.Component {
       <div className="container app" id='app'>
         <MainLayout>
           <Switch>
-            <Route path="/" exact component={Home} />
-            <Route exact path="/login" component={Login} />
+            <Route exact path="/" exact component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/create-post" component={CreatePost} />
             {/* <Route path="/login" exact component={Login} />
             <Route path="/" component={Login} /> */}
             {/* <Route path="/signin" exact component={Login} /> */}
