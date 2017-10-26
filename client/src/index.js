cb9f459d5a54eb0417f522653f36f14e9ef51a22
Zhanwen Chen <phil@zhanwenchen.com>
2017-10-26 18:18:35 -0400

    minor cleanup for src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import './index.css';
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root'));
-// registerServiceWorker();
+  document.getElementById('root')
+);
+registerServiceWorker();
