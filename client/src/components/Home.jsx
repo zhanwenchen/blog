4045bda8af6e322ca18237f2a7ae7a402028f8a6
Zhanwen Chen <phil@zhanwenchen.com>
2017-10-26 18:11:50 -0400

    cleaned up Home.jsx, removed ErrorBoundary, and converted it to stateless

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import ErrorBoundary from '../components/ErrorBoundary';
 import Posts from '../components/Posts';
 
-export default class Home extends React.Component {
-	render() {
-		return (
-			<div className="home" id='home'>
-				<ErrorBoundary>
-					<Posts/>
-				</ErrorBoundary>
-			</div>
-		);
-	}
-}
+const Home = () => (
+  <div className="home" id="home">
+    <Posts />
+  </div>
+);
+
+export default Home;
