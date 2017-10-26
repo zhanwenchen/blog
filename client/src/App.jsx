04fc44e4e9676c40f83b9f7d39463fc32f94c51b
Zhanwen Chen <phil@zhanwenchen.com>
2017-10-26 17:42:28 -0400

    renamed App.js to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
@@ -16,14 +15,6 @@ export default class App extends React.Component {
             <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
             <Route path="/create-post" component={CreatePost} />
-            {/* <Route path="/login" exact component={Login} />
-            <Route path="/" component={Login} /> */}
-            {/* <Route path="/signin" exact component={Login} /> */}
-            {/* <Route path="/signon" exact component={Login} /> */}
-            {/* <Route path="/about" exact component={About} /> */}
-            {/* <Route path="/privacy" exact component={Privacy} /> */}
-            {/* <Route path="/composers" exact component={Composers} /> */}
-            {/* <Route path="/composers/:artist_id" exact component={Composer} /> */}
           </Switch>
         </MainLayout>
       </div>
