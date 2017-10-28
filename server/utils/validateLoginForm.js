febb4d526fc05f64dd767b65f7bfc6c142c6f978
Zhanwen Chen <phil@zhanwenchen.com>
2017-10-28 17:41:10 -0400

    added completed utils/validateLoginForm.js

---
+++ server/utils/validateLoginForm.js
@@ -0,0 +1,29 @@
+/**
+ * @function validateLoginForm
+ * @param {Object} payload
+ */
+module.exports = (payload) => {
+  const errors = {};
+  let isFormValid = true; // REVIEW: is this the best approach for validation?
+  let message = '';
+
+  if (!payload || typeof payload.email !== 'string' || payload.email.trim().length === 0) {
+    isFormValid = false;
+    errors.email = 'Please provide your email address.';
+  }
+
+  if (!payload || typeof payload.password !== 'string' || payload.password.trim().length === 0) {
+    isFormValid = false;
+    errors.password = 'Password provide your password.';
+  }
+
+  if (!isFormValid) {
+    message = 'Check the form for errors.';
+  }
+
+  return {
+    success: isFormValid,
+    message,
+    errors,
+  };
+};
