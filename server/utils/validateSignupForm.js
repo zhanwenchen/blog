c4b0437be5a15a041b33f03b923cffab46096e81
Zhanwen Chen <phil@zhanwenchen.com>
2017-10-28 17:34:59 -0400

    added completed server/utils/validateSignupForm.js

---
+++ server/utils/validateSignupForm.js
@@ -0,0 +1,36 @@
+const validator = require('validator');
+
+/**
+ * @function validateSignupForm
+ * @param {Object} payload
+ */
+module.exports = (payload) => {
+  const errors = {};
+  let isFormValid = false;
+  let message = '';
+
+  if (!payload || typeof payload.name !== 'string' || payload.name.trim().length === 0) {
+    isFormValid = false;
+    errors.name = 'Please provide your name.';
+  }
+
+  if (!payload || typeof payload.email !== 'string' || !validator.isEmail(payload.email)) {
+    isFormValid = false;
+    errors.email = 'Please provide a correct email address.';
+  }
+
+  if (!payload || typeof payload.password !== 'string' || payload.password.trim().length < 8) {
+    isFormValid = false;
+    errors.password = 'Password must have at least 8 charactors.';
+  }
+
+  if (!isFormValid) {
+    message = 'Check the form for errors.';
+  }
+
+  return {
+    success: isFormValid,
+    message,
+    errors,
+  };
+};
