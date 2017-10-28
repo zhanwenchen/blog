6f61e9dd081f45d47508c7934e7ec851934ee7e3
Zhanwen Chen <phil@zhanwenchen.com>
2017-10-28 15:49:35 -0400

    added complement LoginPage.jsx container

---
+++ client/src/containers/LoginPage.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm.jsx';
+
+class LoginPage extends React.Component {
+  /** @constructor */
+  constructor(props) {
+    super(props);
+
+    // set the initial component state
+    this.state = {
+      errors: {},
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+
+    this.processForm = this.processForm.bind(this);
+    this.changeUser = this.changeUser.bind(this);
+  }
+
+  /**
+  * @function processForm
+  * @param {Object} event - JavaScript event object
+  */
+  processForm(event) {
+    // prevent default action. In this case, action is the form submission
+    event.preventDefault();
+
+    console.log('email:', this.state.user.email);
+    console.log('password:', this.state.user.password);
+  }
+
+  /**
+  * @function changeUser
+  * @param {Object} event - the JavaScript DOM event object
+  */
+  changeUser(event) {
+    const field = event.target.name;
+    const user = this.state.user;
+    user[field] = event.target.value;
+
+    this.setState({
+      user,
+    });
+  }
+
+  render() {
+    return (
+      <LoginForm
+        onSubmit={this.processForm}
+        onChange={this.changeUser}
+        errors={this.state.errors}
+        user={this.state.user}
+      />
+    );
+  }
+}
+
+export default LoginPage;
