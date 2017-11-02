8a3ccf468cf1dd5c74252878424128c951d3f441
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-02 17:47:08 -0400

    refactor: moved password hashing and password checking functionalities from passport local strategiesUser class model as methods

--- server/models/User.js
+++ server/models/User.js
@@ -1,84 +1,74 @@
-/**
-  User.js
-  Class model that
-
-  @param sequelize
-  @param Sequelize
-
-  CHANGED: removed salt because bcrypt already incorporates salt in the hash
-*/
+/** @file User.js - Sequelize class model for user */
 
 const Sequelize = require('sequelize');
 const bcrypt = require('bcrypt');
 
+const SALT_ROUNDS = 10;
+
 module.exports =
+  /** @class User */
   class User extends Sequelize.Model {
+    /**
+     * @function init -
+     * @param {Object} sequelize - the sequelize instance
+     */
     static init(sequelize) {
-      return super.init({
-        firstName: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        lastName: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        // also email
-        username: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          unique: true,
-          validate: {
-            isEmail: true,
+      return super.init(
+        {
+          firstName: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          lastName: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          // username is really an email address
+          username: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            },
+          },
+          // hashed password
+          password: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          isActive: {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false,
           },
         },
-        // hashed password
-        password: {
-          type: Sequelize.STRING,
-          allowNull: false,
+        { sequelize,
+          hooks: {
+            // NOTE that creating and updating require options.sequelize
+            beforeCreate: this.hashPasswordBeforeSave,
+            beforeUpdate: this.hashPasswordBeforeSave,
+          },
         },
-        isActive: {
-          type: Sequelize.BOOLEAN,
-          defaultValue: false,
-        },
-      }, { sequelize });
-    }
-
-    static associate(models) {
-      // Using additional options like CASCADE etc for demonstration
-      // Can also simply do Task.belongsTo(models.User);
-      this.hasMany(models.Post, {
-        onDelete: 'CASCADE',
-        foreignKey: {
-          allowNull: false,
-        },
-      });
-
-      // Using additional options like CASCADE etc for demonstration
-      // Can also simply do Task.belongsTo(models.User);
-      this.hasMany(models.Comment, {
-        onDelete: 'CASCADE',
-        foreignKey: {
-          allowNull: false,
-        },
-      });
+      );
     }
 
     /**
-    * @param {string} passport
-    * @return {passwordhash}
-    */
-    static getPasswordHash(password) {
-      return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-      // return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+     * @function hashPasswordBeforeSave
+     */
+    static async hashPasswordBeforeSave(user, options) {
+      try {
+        const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+        return user.setDataValue('password', hash);
+      } catch (error) {
+        return options.sequelize.Promise.reject(error);
+      }
     }
 
     /**
     * @param {string} password
-    * @return {boolean}
+    * @return {Promise(boolean)} whether a passport matches the hash
     */
-    static isPasswordValid(passwordInput) {
-      return bcrypt.compareSync(passwordInput, this.passwordhash);
-      // return bCrypt.compareSync(password, userpass);
+    async isPasswordValid(passwordInput) {
+      return bcrypt.compare(passwordInput, this.passwordhash);
     }
   };
