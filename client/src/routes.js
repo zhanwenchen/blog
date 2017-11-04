81cb2b2058b0712bae49bb180521a5dc28b3f411
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-04 13:53:08 -0400

    feature: (login) add logout logic to react-router routing and 'bounceIfNotLoggedIn' function factory. Did not do the 'DashboardPage' thing for logged-in users, but will make a 'UserPage' but need to add authorization by userId match

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,33 @@
+/**
+ * TODO: implement authorization in addition to authentication (user.token)
+ * must match user id for editing profile.
+ */
 // FIXME: this routes configuration might be deprecated by react-router v4
 import Base from './components/Base.jsx';
 import Home from './components/Home.jsx';
 import LoginPage from './containers/LoginPage.jsx';
 import SignupPage from './containers/SignupPage.jsx';
+// import UserPage from './';
+import Auth from './modules/Auth';
+
+/**
+ * @function bounceIfNotLoggedIn
+ * @description a helper method that wraps around the React Router
+ * "getComponent" route option (that replaces the "component" option)
+ * callback that conditionally returns a component to the route
+ * @param {React.Component} protectedComponent - a react component
+ * @return {function} a react
+ * @example {path: '/protected', getComponent: protectedComponent(ProtectedPage)}
+ */
+const bounceIfNotLoggedIn = (protectedComponent) => {
+  return (location, callback) => {
+    if (Auth.isTokenExist()) {
+      callback(null, protectedComponent);
+    } else {
+      callback(null, LoginPage);
+    }
+  };
+};
 
 const routes = {
   // base component (wrapper for the whole application).
@@ -10,7 +35,7 @@ const routes = {
   childRoutes: [
     {
       path: '/',
-      component: Home,
+      compoenent: Home,
     },
     {
       path: '/login',
@@ -20,6 +45,20 @@ const routes = {
       path: '/signup',
       component: SignupPage,
     },
+    {
+      path: '/logout',
+      // TODO: remove server-side logout?
+      onEnter: (nextState, replace) => {
+        Auth.removeToken();
+
+        // change the current URL to /
+        replace('/');
+      },
+    },
+    // {
+    //   path: '/user/:userId',
+    //   getComponent: bounceIfNotLoggedIn(UserPage),
+    // },
   ],
 };
 
