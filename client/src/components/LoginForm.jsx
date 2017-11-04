04e38dfbfec0ced291cb108649b226b416be4ea9
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-04 13:30:04 -0400

    feature: (login) incorporate new successMessage parameter and display it in the presentational component components/LoginForm.jsx

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -8,12 +8,14 @@ const LoginForm = ({
   onSubmit,
   onChange,
   errors,
+  successMessage,
   user,
 }) => (
   <Card className="container">
     <form action="/" onSubmit={onSubmit}>
       <h2 className="card-heading">Login</h2>
 
+      {successMessage && <p className="success-message">{successMessage}</p>}
       {errors.sumary && <p className="error-message">{errors.summary}</p>}
 
       <div className="field-line">
@@ -52,6 +54,7 @@ LoginForm.propTypes = {
   onChange: PropTypes.func.isRequired,
   errors: PropTypes.object.isRequired,
   user: PropTypes.object.isRequired,
+  successMessage: PropTypes.string.isRequired,
 };
 
 export default LoginForm;
