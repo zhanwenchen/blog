52e932d9e5867919d9c5223e4e9e5a8709b1ca4e
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-04 13:33:10 -0400

    feature: (login) use src/modules/Auth.js to differentiate logged-in users from others and display logout instead of signup+login

--- client/src/components/Base.jsx
+++ client/src/components/Base.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
+import Auth from '../modules/Auth';
 
 const Base = ({ children }) => (
   <div>
@@ -8,10 +9,19 @@ const Base = ({ children }) => (
         <IndexLink to="/">React Blog</IndexLink>
       </div>
 
-      <div className="top-bar-right">
-        <Link to="/login">Log in</Link>
-        <Link to="/signup">Sign up</Link>
-      </div>
+      {Auth.isTokenExist()
+        ? (
+          <div className="top-bar-right">
+            <Link to="/logout">Log out</Link>
+          </div>
+        )
+        : (
+          <div className="top-bar-right">
+            <Link to="/login">Log in</Link>
+            <Link to="/signup">Sign up</Link>
+          </div>
+        )
+      }
 
     </div>
 
