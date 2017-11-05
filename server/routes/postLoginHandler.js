05f125b2137f4c1dd3d921b955bd8c959c6cf38c
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-05 17:24:25 -0500

    refactor: (cosmetic) minor code cleanup in routes/postLoginHandler.js

--- server/routes/postLoginHandler.js
+++ server/routes/postLoginHandler.js
@@ -1,4 +1,5 @@
 const validateLoginForm = require('../utils/validateLoginForm');
+const debug = require('debug')('blog');
 /**
 * Handle AJAX login calls by returning a JSON with status code
 * most local-login logic in ~/configurePassport.js
@@ -8,25 +9,11 @@ const passport = require('passport');
 
 module.exports = (req, res, next) => {
   const validationResult = validateLoginForm(req.body);
-  if (!validationResult.success) {
-    return res.status(400).json(validationResult);
-  }
-  // (err, user, info) => {} is a custom callback instead of
-  // {  successRedirect: '/blah', failureRedirect: '/blahblahh' }
+  if (!validationResult.success) return res.status(400).json(validationResult);
   return passport.authenticate('local-login', (err, token, info) => {
     if (err) return res.status(401).json({ message: 'error authenticating' });
     if (!token) return res.status(401).json(validationResult);
 
-    return res.json(Object.assign(validationResult, { token }));
-
-    // Manually establish the session...
-    // req.logIn(user, (error) => {
-    //   // console.log('In postLoginHandler. user is', user);
-    //   if (error) return next(error);
-    //   return res.status(200).json({
-    //     username: user.username,
-    //     message: 'user authenticated',
-    //   });
-    // });
+    return res.status(200).json(Object.assign(validationResult, { token }));
   })(req, res, next);
 };
