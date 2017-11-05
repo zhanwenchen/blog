4e640fc55a344ab5ad14f5701c887ba2f4635807
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-05 00:26:49 -0400

    bug: (fix) solved 'unable to set headers after they are sent' problem but avoiding passing 'error' to 'done'; add additional catch-done block to handle leaked Sequelize validation error

--- server/passport/localSignupStrategy.js
+++ server/passport/localSignupStrategy.js
@@ -20,16 +20,29 @@ module.exports = new PassportLocalStrategy(
       lastName: req.body.lastName.trim(),
     };
 
+    /**
+     * NOTE: the problem might be that we are returning done twice, once
+     * in then existingUser, another in User.create catch.
+     * NOTE: correction: the above is wrong. You cannot call done(error)
+     * because it crashes the app and there's no way to handle it in
+     * the handler (which can only return next(error) or risk returning
+     * multiple error responses with multiple status codes). Rather, use the
+     * second param (whether a user exists)
+     * as an error condition which the handler can then use to return
+     * a status once
+     */
     User.findOne({ where: { username: email } })
       .then((existingUser) => {
         if (existingUser) {
           return done(null, false, { message: `user with email ${email} already exists` });
         }
-        return existingUser;
+        return null;
       })
       // TODO: important promise lesson: then(() => Promise) instead of then(Promise)
       .then(() => User.create(userData))
-      .catch(error => done(error, false, { message: 'error in local-signup' }))
+      .catch(error =>
+        done(null, false, { message: `error in local-signup: ${error}` }),
+      )
       .then((newUser) => {
         if (newUser) {
           debug('In configurePassport.createUser. newUser was created. newUser is ', newUser);
