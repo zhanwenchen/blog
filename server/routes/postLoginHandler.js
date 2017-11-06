4f246a7264480056a6b179f846a92b90eddae704
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-06 00:27:45 -0500

    bug: (fix) 1. return token in response json

--- server/routes/postLoginHandler.js
+++ server/routes/postLoginHandler.js
@@ -1,5 +1,4 @@
 const validateLoginForm = require('../utils/validateLoginForm');
-const debug = require('debug')('blog');
 /**
 * Handle AJAX login calls by returning a JSON with status code
 * most local-login logic in ~/configurePassport.js
@@ -12,8 +11,11 @@ module.exports = (req, res, next) => {
   if (!validationResult.success) return res.status(400).json(validationResult);
   return passport.authenticate('local-login', (err, token, info) => {
     if (err) return res.status(401).json({ message: 'error authenticating' });
-    if (!token) return res.status(401).json(validationResult);
+    if (!token) return res.status(401).json({ message: 'incorrect credentials' });
 
-    return res.status(200).json(Object.assign(validationResult, { token }));
+    return res.status(200).json({
+      token,
+      message: 'login successful',
+    });
   })(req, res, next);
 };
