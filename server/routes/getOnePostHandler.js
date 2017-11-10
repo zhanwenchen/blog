4e4e21bd6cf8b9d07282af8f79f6bcbb38589fa5
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-10 13:00:17 -0500

    bug: (fix) solved getOnePostHandler and getOneUser 404 error by using 'req.params.<param>' instead of 'req.param.<param>'

--- server/routes/getOnePostHandler.js
+++ server/routes/getOnePostHandler.js
@@ -3,12 +3,14 @@
 */
 
 const models = require('../models');
-
+const debug = require('debug')('blog');
 
 module.exports = (req, res) => {
   // REVIEW: see if findOne is deprecated in favor of findAll()[0]
-  models.Post.findOne({ where: { string_id: req.param.string_id } })
+  debug('getOnePostHandler: req.params is', req.params)
+  models.Post.findOne({ where: { string_id: req.params.string_id } })
     .then((possiblePost) => {
+      debug('possiblePost is', possiblePost)
       if (possiblePost) return res.status(200).json({ post: possiblePost });
       return res.status(404).json(new Error('Post not found'));
     })
--- server/routes/getOneUserHandler.js
+++ server/routes/getOneUserHandler.js
@@ -7,7 +7,7 @@ const models = require('../models');
 
 module.exports = (req, res) => {
   // REVIEW: see if findOne is deprecated in favor of findAll()[0]
-  models.User.findOne({ where: { username: req.param.username } })
+  models.User.findOne({ where: { username: req.params.username } })
     .then((possibleUser) => {
       if (possibleUser) res.json({ possibleUser });
       else res.json(new Error('User not found'));
