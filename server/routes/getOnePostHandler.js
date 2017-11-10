4df897083db2599a18ac08c51e186385cc80a92d
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-10 13:37:46 -0500

    feature: (core) add author name in the getOnePostHandler json output using Sequelize associations. Also use object destructuring to process grabbed 'possiblePost'

--- server/routes/getOnePostHandler.js
+++ server/routes/getOnePostHandler.js
@@ -2,16 +2,30 @@
 * Route handler for router.get('')
 */
 
-const models = require('../models');
 const debug = require('debug')('blog');
+const { Post, User } = require('../models');
 
 module.exports = (req, res) => {
   // REVIEW: see if findOne is deprecated in favor of findAll()[0]
-  debug('getOnePostHandler: req.params is', req.params)
-  models.Post.findOne({ where: { string_id: req.params.string_id } })
+  // debug('getOnePostHandler: req.params is', req.params);
+  Post.findOne({
+    where: { string_id: req.params.string_id },
+    include: [User],
+  })
     .then((possiblePost) => {
-      debug('possiblePost is', possiblePost)
-      if (possiblePost) return res.status(200).json({ post: possiblePost });
+      debug('possiblePost is', possiblePost);
+      if (possiblePost) {
+        const { title, body, createdAt, updatedAt, User: user } = possiblePost;
+        return res.status(200).json({
+          post: {
+            title,
+            body,
+            createdAt,
+            updatedAt,
+            author: `${user.firstName} ${user.lastName}`,
+          },
+        });
+      }
       return res.status(404).json(new Error('Post not found'));
     })
     .catch((err) => {
