2857bde6ca2232cd7ba1ee6b463883ebe9c6bfdb
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-13 00:57:29 -0500

    (Work in Progress) bug: (partial fix) trying to change src/routes.js to a Routes.jsx component. Stuck on logout. Save work in progress for later.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,30 @@ import ReactDom from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { browserHistory, Router } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
-import routes from './routes';
+import Routes from './Routes.jsx';
 import './App.css';
 
 // remove tap delay, essential for MaterialUI to work properly
-injectTapEventPlugin();
+// injectTapEventPlugin();
+//
+// ReactDom.render(
+//   (
+//     <MuiThemeProvider muiTheme={getMuiTheme()}>
+//       <Router history={browserHistory} routes={routes} />
+//     </MuiThemeProvider>
+//   ),
+//   document.getElementById('react-app'),
+// );
 
 ReactDom.render(
   (
-    <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <Router history={browserHistory} routes={routes} />
-    </MuiThemeProvider>
+    <BrowserRouter>
+      <MuiThemeProvider>
+        <Routes />
+      </MuiThemeProvider>
+    </BrowserRouter>
   ),
   document.getElementById('react-app'),
 );
