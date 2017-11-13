e9f8d94411a372a3a9a6c919d3915cb95e426197
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-13 00:11:47 -0500

    change: (data) changed server/routes/getAllPostsHandler.js to use {posts: postsArray} instead just the 'postsArray' as the json output

--- server/routes/getAllPostsHandler.js
+++ server/routes/getAllPostsHandler.js
@@ -1,19 +1,10 @@
 /**
-* Route handler for router.get('/users')
+* @file Route handler for router.get('/users')
 */
-
-const models = require('../models');
-
+const { Post } = require('../models');
 
 module.exports = (req, res) => {
-
-  models.Post.findAll()
-    .then(postsArray => {
-      res.status(200).send(postsArray);
-    })
-    .catch(err => {
-      res.json({
-        message: err
-      })
-    })
+  Post.findAll()
+    .then(postsArray => res.status(200).send({ posts: postsArray }))
+    .catch(err => res.json({ message: err }));
 };
