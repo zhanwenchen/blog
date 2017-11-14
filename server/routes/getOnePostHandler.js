549d11b55bbcc2bd9476e076dfab91e7ae8321c9
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-13 19:23:00 -0500

    change: (data) have find post handlers send raw data without reformatting. Move the data cleaning to the client side

--- server/routes/getOnePostHandler.js
+++ server/routes/getOnePostHandler.js
@@ -2,33 +2,19 @@
 * Route handler for router.get('')
 */
 
-const debug = require('debug')('blog');
 const { Post, User } = require('../models');
 
 module.exports = (req, res) => {
-  // REVIEW: see if findOne is deprecated in favor of findAll()[0]
-  // debug('getOnePostHandler: req.params is', req.params);
   Post.findOne({
     where: { string_id: req.params.string_id },
     include: [User],
   })
-    .then((possiblePost) => {
-      debug('possiblePost is', possiblePost);
-      if (possiblePost) {
-        const { title, body, createdAt, updatedAt, User: user } = possiblePost;
-        return res.status(200).json({
-          post: {
-            title,
-            body,
-            createdAt,
-            updatedAt,
-            author: `${user.firstName} ${user.lastName}`,
-          },
-        });
-      }
-      return res.status(404).json(new Error('Post not found'));
-    })
-    .catch((err) => {
-      res.json({ message: err });
-    });
+    .then(possiblePost =>
+      (
+        possiblePost ?
+          res.status(200).json({ errors: {}, post: possiblePost }) :
+          res.status(404).json({ errors: 'post not found', post: null })
+      ),
+    )
+    .catch(error => res.json({ errors: error }));
 };
