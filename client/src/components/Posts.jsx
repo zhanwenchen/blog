2513c8da6ae8750d4e7f393dc8bcbc35764a278a
Zhanwen Chen <phil@zhanwenchen.com>
2017-11-13 19:41:06 -0500

    warning: (fix) resolved react 'key' warning for Posts.jsx mapped 'post's by adding a key={post.string_id} prop. Also modified 'author' to use 'post.User.firstName+ post.User.lastName'

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -10,21 +10,23 @@ const Posts = ({
   posts,
 }) => (
   <div className="posts">
-    <p>here</p>
     {/* {errors.summary && <p className="error-message">{errors.summary}</p>} */}
     {posts.length === 0 && <p>There are currently no posts</p>}
     {posts.length > 0 && <p> what </p> &&
       posts.map(post => (
         <Post
+          key={post.string_id}
+          errors={errors}
           stringId={post.string_id}
-          author={post.author}
+          author={`${post.User.firstName} ${post.User.lastName}`}
           title={post.title}
           body={_.truncate(post.body, {
             length: TEASER_NUM_OF_CHARS,
             separator: /,?\.* +/,
           })}
-          createdAt={post.createdAt}
-          updatedAt={post.updatedAt}
+          // TODO: find a more performant way to format date strings
+          createdAt={new Date(post.createdAt).toString()}
+          updatedAt={new Date(post.updatedAt).toString()}
         />
       ))
     }
@@ -37,53 +39,3 @@ Posts.propTypes = {
 };
 
 export default Posts;
-// const propTypes = {
-// 	match: PropTypes.object.isRequired,
-// 	location: PropTypes.object.isRequired,
-// 	history: PropTypes.object.isRequired,
-// };
-//
-// export default class Posts extends React.Component {
-//
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = { posts: [] };
-// 	}
-//
-// 	componentDidMount() {
-// 		// console.log('component did mount in Composer');
-// 		// console.log(this)
-// 		// console.log(`The value of posts before the fetch is ${this.state.posts}`);
-// 		// fetch('/api/posts')
-// 		// 	.then(res => res.json())
-// 		// 	.then(posts => this.setState({ posts }))
-// 		// 	.then(() => { console.log(`The value of composer after the fetch is ${this.state.settings}`) })
-// 	}
-//
-// 	render() {
-// 		// Initially the composer is null before data is fetched.
-// 		return (
-// 			<div className="posts">
-// 				<div className="settings">
-// 					{this.state.posts.length === 0 &&
-// 						<div>
-// 							<p>There are currently no posts</p>
-// 						</div>
-// 					}
-// 					{this.state.posts.length > 0 &&
-// 						this.state.posts.map(post => (
-// 							<div className="post" key={post.string_id}>
-// 								<Link to={`${this.props.location.pathname + '/' + post.string_id}`}>
-// 									{_.truncate(post.body, {
-// 										length: TEASER_NUM_OF_CHARS,
-// 										separator: /,?\.* +/,
-// 									})}
-// 								</Link>
-// 							</div>
-// 						))
-// 					}
-// 			</div>
-// 		</div>
-// 		);
-// 	}
-// }
